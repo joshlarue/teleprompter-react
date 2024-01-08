8a37ffb112e0502e8ff17a4783f2677a6079b3c9
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-01-07 18:12:28 -0700

    can record and download but only in webm format. Not sure why

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 function App() {
   // adapted from https://www.geeksforgeeks.org/how-to-open-web-cam-in-javascript/
   document.addEventListener("DOMContentLoaded", () => {
+    let mediaRecorder;
+    let body = document.querySelector(".body");
     let camStream = document.querySelector(".camera-stream");
     let playBtn = document.querySelector(".camera-start");
     let recBtn = document.querySelector(".record-btn");
@@ -10,74 +12,65 @@ function App() {
     let mediaDevices = navigator.mediaDevices;
     camStream.muted = true;
 
-    playBtn.addEventListener("click", () => {
-      mediaDevices
-        .getUserMedia({video: true, audio: true,})
-        .then((stream) => {
-          camStream.srcObject = stream;
-          camStream.addEventListener("loadedmetadata", () => {
-            camStream.play();
-          });
-          const mediaRecorder = new MediaRecorder(stream);
-
-          recBtn.addEventListener("click", () => {
-            mediaRecorder.start();
-            console.log(mediaRecorder.state);
-            recBtn.style.background="red";
-          });
-
-
-          // based on https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API
-          let chunks = [];
-
-          mediaRecorder.ondataavailable = (e) => {
-            chunks.push(e.data);
-          }
-
-          stopBtn.addEventListener("click", () => {
-            mediaRecorder.stop()
-            console.log(mediaRecorder.state);
-            recBtn.style.background = '';
-          });
-
-          mediaRecorder.onstop = (e) => {
-            console.log("recorder stopped.");
-
-            const clip = prompt("filename for clip: ");
-
-            const clipContainer = document.createElement("article");
-            const clipLabel = document.createElement("p");
-            const audio = document.createElement("audio");
-            const displayVid = document.createElement("video");
-            const delBtn = document.createElement("button");
-
-            clipContainer.classList.add("clip");
-            delBtn.textValue = 'delete';
-            clipLabel.textValue = clip;
-
-            clipContainer.appendChild(displayVid);
-            clipContainer.appendChild(audio);
-            clipContainer.appendChild(clipLabel);
-            clipContainer.appendChild(delBtn);
-
-            const blob = new Blob(chunks, { type: "video/mp4"});
-            chunks = [];
-            const vidURL = window.URL.createObjectURL(blob);
-            displayVid.src = vidURL;
-          }
-
+    mediaDevices.getUserMedia({video: true, audio: true,})
+      .then((stream) => {
+        camStream.srcObject = stream;
+        camStream.addEventListener("loadedmetadata", () => {
+          camStream.play();
         })
-        .catch((alert) => {
-          console.log(`The following error occured: ${alert}`);
-        });
+        mediaRecorder = new MediaRecorder(stream);
+
+        mediaRecorder.ondataavailable = (e) => {
+          chunks.push(e.data);
+        }
+
+        mediaRecorder.onstop = () => {
+          console.log("recorder stopped.");
+    
+          const clip = 'test';
+    
+          const clipContainer = document.createElement("div");
+          const clipLabel = document.createElement("p");
+          const audio = document.createElement("audio");
+          const displayVid = document.createElement("video");
+          const dlLink = document.createElement("a");
+    
+          clipLabel.textContent = clip;
+    
+          const blob = new Blob(chunks, { type: "video/mp4" });
+          chunks = [];
+          const vidURL = window.URL.createObjectURL(blob);
+          displayVid.src = vidURL;
+    
+          dlLink.href = vidURL;
+          dlLink.download = `${clip}.mp4`;
+          dlLink.textContent = 'Download video';
+          clipContainer.appendChild(displayVid);
+          clipContainer.appendChild(clipLabel);
+          clipContainer.appendChild(dlLink);
+          body.appendChild(clipContainer);
+        }
+      });
+
+    recBtn.addEventListener("click", () => {
+      mediaRecorder.start();
+      console.log(mediaRecorder.state);
+      recBtn.style.background="red";
+    });
+    // based on https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API
+    let chunks = [];
+
+    stopBtn.addEventListener("click", () => {
+      mediaRecorder.stop()
+      console.log(mediaRecorder.state);
+      recBtn.style.background = '';
     });
 
+    
   });
 
-
-
   return (
-    <>
+    <div className='body'>
       <video className="camera-stream"></video>
       <button className="camera-start" autoPlay>
         Start Webcam
@@ -88,8 +81,8 @@ function App() {
       <button className="stop-btn">
         Stop
       </button>
-    </>
+    </div>
   )
 }
 
-export default App
+export default App;
